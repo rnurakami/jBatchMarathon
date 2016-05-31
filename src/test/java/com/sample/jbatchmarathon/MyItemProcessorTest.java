e26ecb7c4664670a7b08f6e39cfe387630055618
akira_abe <>
2016-05-31 18:06:29 +0900

    一部ユニットテストを記述しました。 JMockitの技術検証も兼ねています。

---
+++ src/test/java/com/sample/jbatchmarathon/MyItemProcessorTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sample.jbatchmarathon;
+
+import com.sample.jbatchmarathon.domain.Race;
+import com.sample.jbatchmarathon.domain.Runner;
+import com.sample.jbatchmarathon.util.CalendarUtil;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author akira_abe
+ */
+public class MyItemProcessorTest {
+    
+    public MyItemProcessorTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * ItemProcessorのテストメソッドです。
+     */
+     @Test
+     public void testProcessSuccess() {
+         
+         MyItemProcessor proc = new MyItemProcessor();
+         
+         // 入力のオブジェクトを生成します。
+         Runner run1 = new Runner();
+         run1.setId(1001);
+         run1.setName("akira_abe");
+         run1.setBirthDay(CalendarUtil.toDate("1968-1-5"));
+         Race race11 = new Race();
+         race11.setName("河口湖マラソン");
+         race11.setDate(CalendarUtil.toDate("2006-11-23"));
+         race11.setDistance(42.195);
+         race11.setResult("04h30m00s");
+         Race race12 = new Race();
+         race12.setName("東京マラソン");
+         race12.setDate(CalendarUtil.toDate("2007-2-10"));
+         race12.setDistance(42.195);
+         race12.setResult("05h00m00s");
+         List<Race> races1 = Arrays.asList(race11,race12);
+         run1.setRaces(races1);
+         
+        try {
+            proc.processItem(run1);
+        } catch (Exception ex) {
+            Logger.getLogger(MyItemProcessorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //TODO:現状Processが特に処理をしていないので、何らかの仕様を実装したら、検証をする。
+     }
+}
--- src/test/java/com/sample/jbatchmarathon/MyItemReaderTest.java
+++ src/test/java/com/sample/jbatchmarathon/MyItemReaderTest.java
@@ -1,30 +1,104 @@
 package com.sample.jbatchmarathon;
 
+import com.sample.jbatchmarathon.domain.Runner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.batch.runtime.context.JobContext;
+import static mockit.Deencapsulation.setField;
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import mockit.integration.junit4.JMockit;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
 
 /**
  * MyItemReaderのテストクラスです。
+ * <pre>
+ * ReaderはJobContextに依存するため、モック（JMockit）を利用しています。
+ * </pre>
+ *
  * @author akira_abe
  */
+@RunWith(JMockit.class)
 public class MyItemReaderTest {
-    
+
+    // Testeeが依存するJobContextをモック化します。
+    @Mocked
+    private JobContext jobContext;
+
     public MyItemReaderTest() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    /**
+     * MyItemReaderのテストメソッドです。
+     */
+    @Test
+    public void testRead() {
+
+        // Testeeをインスタンス化します。
+        MyItemReader reader = new MyItemReader();
+
+        // Mockが返却する値を設定します。
+        final String fileName = "c:/temp/input.csv";
+        
+        // Mockの振る舞いを記述します。
+        new NonStrictExpectations() {
+            {
+                jobContext.getProperties().getProperty("input_file");
+                result = fileName;
+            }
+        };
+        // privateフィールドへモックを差し込みます。
+        setField(reader, jobContext);
+        
+        // ファイルをOpenします。
+        try {
+            reader.open("100");
+        } catch (Exception ex) {
+            Logger.getLogger(MyItemReaderTest.class.getName()).log(Level.SEVERE, "★★★ file open エラーが発生しました。", ex);
+        }
+
+        // ファイルをReadします。
+        try {
+            // 試作品なので大雑把にassertしています。
+            Runner run1 = (Runner) reader.readItem();
+            assertThat(run1.getName(),is("akira_abe"));
+            assertThat(run1.getRaces().size(),is(2));
+            System.out.println(run1);
+            
+            Runner run2 = (Runner) reader.readItem();
+            assertThat(run2.getName(),is("Haile Gebrselassie"));
+            assertThat(run2.getRaces().get(0).getResult(),is("02h04m26s"));
+            System.out.println(run2);
+            
+            Runner run3 = (Runner) reader.readItem();
+            assertThat(run3, nullValue());
+            System.out.println(run3);
+
+        } catch (Exception ex) {
+            Logger.getLogger(MyItemReaderTest.class.getName()).log(Level.SEVERE, "★★★ readエラーが発生しました。", ex);
+        }
+
+        // ファイルをCloseします。
+        try {
+            reader.close();
+        } catch (Exception ex) {
+            Logger.getLogger(MyItemReaderTest.class.getName()).log(Level.SEVERE, "★★★ file close エラーが発生しました。", ex);
+        }
+        
+    }
 }
